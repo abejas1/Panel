a68f6be1e27480ff6e979a78f0203e8e48f42824
Abejas <abejas.1.2000@gmailcom>
2018-06-26 20:01:00 +0000

    Balanza

--- Abejas/peso/peso.ino
+++ Abejas/peso/peso.ino
@@ -1,18 +1,20 @@
+
 #include "HX711.h"
 
-#define DOUT  A1
-#define CLK  A0
-
-HX711 balanza(DOUT, CLK);
+HX711 balanza1(A1, A0);
+HX711 balanza2(A3, A2);
 
 void setup() {
   Serial.begin(9600);
   Serial.print("Lectura del valor del ADC:  ");
-  Serial.println(balanza.read());
+  Serial.println(balanza1.read()+balanza2.read());
+
   Serial.println("No ponga ningun  objeto sobre la balanza");
   Serial.println("Destarando...");
-  balanza.set_scale(); //La escala por defecto es 1
-  balanza.tare(20);	//El peso actual es considerado Tara.
+  balanza1.set_scale(); //La escala por defecto es 1
+  balanza1.tare(20);	//El peso actual es considerado Tara.
+  balanza2.set_scale();
+  balanza2.tare(20);
   Serial.println("Coloque un peso conocido:");
   
 }
@@ -20,9 +22,9 @@ void setup() {
 
 void loop() {
 
+  
   Serial.print("Valor de lectura:  ");
-  Serial.println(balanza.get_value(10),0);
+  Serial.println(balanza1.get_value(10)+balanza2.get_value(10),0);
   delay(100);
 }
-
-
+	
